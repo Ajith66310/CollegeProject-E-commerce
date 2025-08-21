26052987d685071f062867fb3ed607286aaafd6e
Ajith k v <ajith66310@gmail.com>
2025-08-21 19:04:33 +0530

    fix: add methods in cors

--- backend/server.js
+++ backend/server.js
@@ -21,20 +21,19 @@ const allowedOrigins = [
   "http://localhost:5173", // frontend
   "http://localhost:5174", // admin
 ];
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (like mobile apps, curl)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      } else {
-        return callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 
 app.use("/api/user", userRouter);
 app.use("/api/product", productRouter);
