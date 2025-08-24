17c71147094fc1593e96e415fe782161bc937128
Ajith k v <ajith66310@gmail.com>
2025-08-24 15:23:54 +0530

    fix: import backendurl in product.jsx from .env

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -38,7 +38,7 @@ const Product = () => {
     if (!reviewText.trim()) return toast("Review cannot be empty");
 
     try {
-      const res = await fetch(`${VITE_BACKEND_URL}/api/product/${productData._id}/review/add`, {
+      const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/product/${productData._id}/review/add`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
